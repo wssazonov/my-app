f3f680e30ea70fba7ade7b8cf6724bd9cbb3fd6f
Viacheslav Sazonov <viacheslav.sazonov@seagullinc.co>
2020-05-19 23:50:39 +0300

    Updates

---
+++ precache-manifest.6991c08ca42af5a9a8ca6f0d1c72b542.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "489f928ac6cbd02f95e66fb88da08405",
+    "url": "/my-app/def-master/index.html"
+  },
+  {
+    "revision": "f900b36e6373be8664c1",
+    "url": "/my-app/def-master/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "993847e43307f62ce5d7",
+    "url": "/my-app/def-master/static/css/main.8b0fb017.chunk.css"
+  },
+  {
+    "revision": "f900b36e6373be8664c1",
+    "url": "/my-app/def-master/static/js/2.7f5a7d6d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-app/def-master/static/js/2.7f5a7d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "993847e43307f62ce5d7",
+    "url": "/my-app/def-master/static/js/main.17f7dc59.chunk.js"
+  },
+  {
+    "revision": "729e75770a3e610d5712",
+    "url": "/my-app/def-master/static/js/runtime-main.8ab0bd4d.js"
+  },
+  {
+    "revision": "029e71ddd366d629e61cfec0e02a5f2f",
+    "url": "/my-app/def-master/static/media/add_button.029e71dd.png"
+  },
+  {
+    "revision": "ec710cd9ef70a28c86fcb460f24ba19c",
+    "url": "/my-app/def-master/static/media/beer.ec710cd9.png"
+  },
+  {
+    "revision": "4d85f810d024aa19c00a0d0d2246ddb5",
+    "url": "/my-app/def-master/static/media/callme.4d85f810.png"
+  },
+  {
+    "revision": "53486309973853d51d1bc5152f553077",
+    "url": "/my-app/def-master/static/media/champagne.53486309.png"
+  },
+  {
+    "revision": "35046ef3fd96b4285ccbbcc56ede8267",
+    "url": "/my-app/def-master/static/media/cognac.35046ef3.png"
+  },
+  {
+    "revision": "fe95fa4ead682c44f053f93956ef4214",
+    "url": "/my-app/def-master/static/media/liquor.fe95fa4e.png"
+  },
+  {
+    "revision": "2bca7c5ffdb57fe5e0cef2734d6d1cde",
+    "url": "/my-app/def-master/static/media/logo.2bca7c5f.png"
+  },
+  {
+    "revision": "99cb7b10ecc0d24bfb708068d7306d30",
+    "url": "/my-app/def-master/static/media/pay.99cb7b10.png"
+  },
+  {
+    "revision": "968ec8d2d596378d3d0782127d502b0c",
+    "url": "/my-app/def-master/static/media/pic1.968ec8d2.png"
+  },
+  {
+    "revision": "5d451842f9e99be2dfcf8207a1e4edbb",
+    "url": "/my-app/def-master/static/media/pic2.5d451842.png"
+  },
+  {
+    "revision": "0f0809ebe9addb57693ab12083f890be",
+    "url": "/my-app/def-master/static/media/pic3.0f0809eb.png"
+  },
+  {
+    "revision": "1862e65a6723bc0392399bc5fbd20eb0",
+    "url": "/my-app/def-master/static/media/pic4.1862e65a.png"
+  },
+  {
+    "revision": "14785f0a43e86f39f805c0c7fba2ddbb",
+    "url": "/my-app/def-master/static/media/rum.14785f0a.png"
+  },
+  {
+    "revision": "11ff2bb1974863644640046ae24b3d70",
+    "url": "/my-app/def-master/static/media/sale.11ff2bb1.png"
+  },
+  {
+    "revision": "156599e39dc7b8b394ce8210a3c0501d",
+    "url": "/my-app/def-master/static/media/save.156599e3.png"
+  },
+  {
+    "revision": "5def1e3b87911d0a063e7b9c1b4c9c0a",
+    "url": "/my-app/def-master/static/media/save_number.5def1e3b.png"
+  },
+  {
+    "revision": "4308a279418f31748e56a6416f9124e9",
+    "url": "/my-app/def-master/static/media/signup.4308a279.png"
+  },
+  {
+    "revision": "5d836ecd1768227923b82d6158a9840b",
+    "url": "/my-app/def-master/static/media/slider.5d836ecd.png"
+  },
+  {
+    "revision": "49f07123885acccf7c1371942b218f91",
+    "url": "/my-app/def-master/static/media/snack.49f07123.png"
+  },
+  {
+    "revision": "5573daa23dac33258f259b256740bc25",
+    "url": "/my-app/def-master/static/media/vodka.5573daa2.png"
+  },
+  {
+    "revision": "4575d23464952d9c9b5da615a8abf22c",
+    "url": "/my-app/def-master/static/media/whiskey.4575d234.png"
+  },
+  {
+    "revision": "72cf7f828e6591e6722f07ed9e019b85",
+    "url": "/my-app/def-master/static/media/wine.72cf7f82.png"
+  }
+]);
